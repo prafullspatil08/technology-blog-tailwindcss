b4f7cf222312e85ba6cdb8200f07d147b4059327
rushikesh-salunke <rushikesh.salunke@mindbowser.com>
2023-05-01 21:54:15 +0530

    generated shared & dashboard modules component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'technology-blog'`, () => {
+  it(`should have as title 'pharma-care'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('technology-blog');
+    expect(app.title).toEqual('pharma-care');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('technology-blog app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('pharma-care app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'technology-blog';
+  title = 'pharma-care';
 }
