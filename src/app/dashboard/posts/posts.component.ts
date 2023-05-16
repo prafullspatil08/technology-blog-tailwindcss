07f607a8200b2829f657200e7d22ece363cc187e
rushikesh-salunke <rushikesh.salunke@mindbowser.com>
2023-05-16 16:15:10 +0530

    post component

--- src/app/dashboard/posts/posts.component.ts
+++ src/app/dashboard/posts/posts.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss']
+  styleUrls: ['./posts.component.scss'],
 })
-export class PostsComponent {
-
-}
+export class PostsComponent {}
