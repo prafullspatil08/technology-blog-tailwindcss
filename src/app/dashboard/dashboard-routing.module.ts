611e5e5b8448291b59ed85cf779a1047f255bd8a
rushikesh-salunke <rushikesh.salunke@mindbowser.com>
2023-05-16 16:30:08 +0530

    post project structure

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
-import { PostsComponent } from './posts/posts.component';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { PostComponent } from './posts/post/post.component';
 
 const routes: Routes = [
   {
@@ -11,7 +11,7 @@ const routes: Routes = [
     component: DashboardComponent,
     children: [
       { path: '', redirectTo: 'posts', pathMatch: 'full' },
-      { path: 'posts', component: PostsComponent },
+      { path: 'post', component: PostComponent },
       { path: 'sign-in', component: SignInComponent },
       { path: 'sign-up', component: SignUpComponent },
     ],
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,18 +4,18 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { DashboardComponent } from './dashboard.component';
-import { PostsComponent } from './posts/posts.component';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PostComponent } from './posts/post/post.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    PostsComponent,
     SignInComponent,
-    SignUpComponent
+    SignUpComponent,
+    PostComponent
   ],
   imports: [
     CommonModule,
